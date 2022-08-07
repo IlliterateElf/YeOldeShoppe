46f4d852081a4ad6a6fb2f592bd5a260e6ca9c13
IlliterateElf <illiterate.elf@outlook.com>
2022-08-07 14:32:55 -0700

    get shop item function

--- Shoppe/views.py
+++ Shoppe/views.py
@@ -4,3 +4,7 @@ from Shoppe.models import ShopItem
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def get_shopp_item(request, pk):
+    shoppe_objects = ShopItem.objects.get(pk=pk)
+    return render(request, "index.html")
