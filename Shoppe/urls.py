868afbee66a035290e187f9e31e912678e698477
IlliterateElf <illiterate.elf@outlook.com>
2022-08-07 20:44:13 -0700

    index is now search, new index, added searching
    Rationality is index is the primary webpage to search whatever the user wants to buy.
    
    Once the user searches, the form passes to searching where it essentially redirects to the dynamic URL for search.html.
    
    Search.html now has a dynamic URL path of the actual query and displays query in webpage.

--- Shoppe/urls.py
+++ Shoppe/urls.py
@@ -5,7 +5,9 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('searching', views.searching, name='searching'),
+    path('search+q=<str:pk>', views.search, name='search')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- Shoppe/views.py
+++ Shoppe/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from Shoppe.models import ShopItem
+from django.shortcuts import HttpResponse
 
 # Create your views here.
 def index(request):
-    i = 0
-    shop_objects = ShopItem.objects.all()[:20]
-    length = len(shop_objects)
+    return render(request, 'index.html')
+
+def searching(request):
+    query = request.POST['textField']
+    return redirect('search', query)
+
+def search(request, pk):
+    query = pk
+    shop_objects = ShopItem.objects.filter(name__contains=query)[:20]
     context = {
-        'shop_objects': shop_objects
+        'shop_objects': shop_objects,
+        'query': query
     }
-    return render(request, 'index.html', context)
+    return render(request, 'search.html', context)
